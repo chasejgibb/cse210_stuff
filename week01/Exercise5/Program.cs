c58101fb4d662e18642154d262361eb8034707d9
Chase Gibb <exspartner@gmail.com>
2025-04-28 00:08:32 -0400

    Made the function program!

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Hello world! :)");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Enter your username: ");
+            string username = Console.ReadLine();
+            return username;
+        }
+        
+        static int PromptUserNumber(string username)
+        {
+            Console.Write($"What's your favorite number, {username}? ");
+            int favNum = int.Parse(Console.ReadLine());
+            return favNum;
+        }
+
+        static int SquareNumber(int number)
+        {
+            int sqrdNum = number * number;
+            return sqrdNum;
+        } 
+
+        static void DisplayResult(string username, int sqrdNum)
+        {
+            Console.WriteLine($"{username}, the square of your number is {sqrdNum}");
+        }
+
+        static void Main()
+        {
+            DisplayWelcome();
+            string username = PromptUserName();
+            int num = PromptUserNumber(username);
+            int sqrdNum = SquareNumber(num);
+            DisplayResult(username, sqrdNum);
+        }
+        Main();
     }
 }
