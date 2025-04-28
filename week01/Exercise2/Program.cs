fa80a8dacdd5119e946182758f09994b7751fb47
Chase Gibb <exspartner@gmail.com>
2025-04-27 21:34:39 -0400

    Added if / Else If statements to grade assignment, and pass/fail if conditions

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -2,8 +2,42 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Enter your grade as an integer: ");
+        string gradeStr = Console.ReadLine();
+        int grade = int.Parse(gradeStr);
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        } 
+        else if (grade >= 80 && grade < 90)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            letter = "D";
+        }
+        else if (grade < 60)
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Grade: {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congragulations, you didn't fail! You should be SO proud of yourself");
+        }
+        else
+        {
+            Console.WriteLine("You uh.. didn't pass. Better luck next time.");
+        }
     }
 }
