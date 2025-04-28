bba916e6ce4b9662c2ae64538f271e35edeafae0
Chase Gibb <exspartner@gmail.com>
2025-04-28 16:58:01 -0400

    Completed the Resumes Abstraction learning activity

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "SkyNet";
+        job1._startYear = 2020;
+        job1._endYear = 2025;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Candy Man";
+        job2._company = "tHe WOnKa fAcToRy";
+        job2._startYear = 1001;
+        job2._endYear = 3004;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Average Intern";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
     }
 }
