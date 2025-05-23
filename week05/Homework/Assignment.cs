8084b48eca7945cdee3860d617a08753ffa3912d
Chase Gibb <exspartner@gmail.com>
2025-05-23 13:55:53 -0400

    Inheritance learning assignment

---
+++ week05/Homework/Assignment.cs
@@ -0,0 +1,17 @@
+public class Assignment
+{
+    private string _topic;
+    protected string _studentName;
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    public string GetSummary()
+    {
+        return $"{_studentName} - {_topic}";
+    }
+
+}
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment mathHomework = new MathAssignment("Charlie", "Algebra", "8.3", "1-12");
+        Console.WriteLine(mathHomework.GetSummary());
+        Console.WriteLine(mathHomework.GetHomeworkList());
+
+        WritingAssignment englishHomework = new WritingAssignment("Stephen King", "Creative Writing", "Horror Stories for Toddlers");
+        Console.WriteLine(englishHomework.GetSummary());
+        Console.WriteLine(englishHomework.GetWritingInformation());
     }
 }
+
+
+
+
+
+
+
+
+
+
